5480de9063ada2b3796747ca3f7b8348969e79a7
inspi2k <inspi@hanmail.net>
2023-11-07 16:26:59 +0900

    message update

--- nvshop.py
+++ nvshop.py
@@ -10,18 +10,20 @@ import pyautogui
 
 
 # 0. 검색할 상품 정보 찾기 위해 MID 추출, keyword 얻기
-default_store = "FOL."
-default_product = "FOL 보카 여성 청결제"
+default_store = ""
+default_product = ""
 
-default_mid = "86868741371"
+default_mid = ""
 
-default_keyword = "여성청결제"
+default_keyword = ""
 
 get_mid = ""
 opendate = ""
 result = "\n"
 
-# 1-1. 입력값 받기 (mide / storename, title)
+# 1-1. 입력값 받기 (mid / storename, title)
+# 입력받은 storename - 네이버 쇼핑에서 검색했을 때 상품이 노출되는 스토어명
+# 입력받은 title - 검색 결과에서 구분 가능한 상품명 일부 또는 전체
 check_mid = pyautogui.confirm("상품의 MID를 알고계십니까?", "상품 순위 찾기 (Step 1)")
 if check_mid == "OK":
     get_mid = pyautogui.prompt(
@@ -54,103 +56,116 @@ elif check_mid == "Cancel":
     pageIndex = 1
     pageSize = 40
     encText = urllib.parse.quote(get_store)
+    got_it = False
 
-    headers = {
-        "authority": "search.shopping.naver.com",
-        "accept": "application/json, text/plain, */*",
-        "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-        # 'cookie': 'NNB=XFAKOM3FWBEGG; ASID=3d63aeda00000183d9845de40000005f; autocomplete=use; AD_SHP_BID=18; _ga=GA1.2.1834935952.1667395532; SHP_BUCKET_ID=3; NSCS=1; nid_inf=-1414768515; NID_JKL=FicUSGqv2v1SQhZ86S5KYd02RYYknV1144+ObRT4k5s=; NID_AUT=tClj21DEE4vFWK+w9nNPBu3hDbKtmXdnCP2c/6SJT4dEOkjDxncwG3QryGaHXvNa; nx_ssl=2; _naver_usersession_=zDgBtoiR/jwCgOD7eSV4Yg==; NID_SES=AAABpZRkmxvTsNch089VjaxNIHFLfSw7p10oQTZD55oDBDLmwpK7widsK9FvMAV7MJlib1L/qCeWtdwZYk6MACz2BK2ClK7b2kRGMx6yvK0alZfbyv4Lsqp9VpmBPj0DlnGC5mYZ8/U9FSI+3k6BNf14Yni8d399yEBgMDSSbfbVWk13Ga5ZRBnVlBrCTmZYRPYqllalcsDWYNUUiGvd1jhlCAG/r0EXgZLxK9rspFkJXgMKkSAxmXT2AMeSVVkmhjz6arTrZf/1NGaQINrF4I4ttI0xjRyjd2SX905tXmgBP3ROzlTPPaGDeO0JI7RtEe99zg1rhisCNBJnMBJ9EUswOBMfvm3gmps5CxhBjNSzjToFA7j02qPU5IdYBoPqOPXs1/KDc5rZxuiC/It3gRwA7gPnbEFwYq3AvNr4UxlLHTEFskGpikQkEaZfyF+pwq1Mq2ePM8ZRpQxFlhBIhFx438b59q04mwMKhjKSfKzVtdAYGU8b4JSj3zpVtlPNX4GaAer9eipSQw1319GVneOR79yRpUL938biXG05hP7CY0hs3LrICbQ88uDAoe/XplY2MA==; page_uid=ieavrlp0Jy0ssl5A4Jwsssssths-304147; spage_uid=ieavrlp0Jy0ssl5A4Jwsssssths-304147',
-        "dnt": "1",
-        "logic": "PART",
-        "referer": "https://search.shopping.naver.com/search/all?frm=NVSHATC&origQuery="
-        + encText
-        + "&pagingIndex="
-        + str(pageIndex)
-        + "&pagingSize="
-        + str(pageSize)
-        + "&productSet=total&query="
-        + encText
-        + "&sort=date&timestamp=&viewType=list",
-        "sbth": "6bc7553b8fa3e04779448fd212e42bb46f18162e3d9873ba0d1b41409eae93a803b742c0cb6bfdbfc7737fc581f7e8eb",
-        "sec-ch-ua": '"Whale";v="3", "Not-A.Brand";v="8", "Chromium";v="114"',
-        "sec-ch-ua-arch": '"arm"',
-        "sec-ch-ua-bitness": '"64"',
-        "sec-ch-ua-full-version-list": '"Whale";v="3.21.192.22", "Not-A.Brand";v="8.0.0.0", "Chromium";v="114.0.5735.138"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-model": '""',
-        "sec-ch-ua-platform": '"macOS"',
-        "sec-ch-ua-platform-version": '"13.6.0"',
-        "sec-ch-ua-wow64": "?0",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Whale/3.21.192.22 Safari/537.36",
-    }
+    while (got_it == False) and (pageIndex <= 100):
+        headers = {
+            "authority": "search.shopping.naver.com",
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+            # 'cookie': 'NNB=XFAKOM3FWBEGG; ASID=3d63aeda00000183d9845de40000005f; autocomplete=use; AD_SHP_BID=18; _ga=GA1.2.1834935952.1667395532; SHP_BUCKET_ID=3; NSCS=1; nid_inf=-1414768515; NID_JKL=FicUSGqv2v1SQhZ86S5KYd02RYYknV1144+ObRT4k5s=; NID_AUT=tClj21DEE4vFWK+w9nNPBu3hDbKtmXdnCP2c/6SJT4dEOkjDxncwG3QryGaHXvNa; nx_ssl=2; _naver_usersession_=zDgBtoiR/jwCgOD7eSV4Yg==; NID_SES=AAABpZRkmxvTsNch089VjaxNIHFLfSw7p10oQTZD55oDBDLmwpK7widsK9FvMAV7MJlib1L/qCeWtdwZYk6MACz2BK2ClK7b2kRGMx6yvK0alZfbyv4Lsqp9VpmBPj0DlnGC5mYZ8/U9FSI+3k6BNf14Yni8d399yEBgMDSSbfbVWk13Ga5ZRBnVlBrCTmZYRPYqllalcsDWYNUUiGvd1jhlCAG/r0EXgZLxK9rspFkJXgMKkSAxmXT2AMeSVVkmhjz6arTrZf/1NGaQINrF4I4ttI0xjRyjd2SX905tXmgBP3ROzlTPPaGDeO0JI7RtEe99zg1rhisCNBJnMBJ9EUswOBMfvm3gmps5CxhBjNSzjToFA7j02qPU5IdYBoPqOPXs1/KDc5rZxuiC/It3gRwA7gPnbEFwYq3AvNr4UxlLHTEFskGpikQkEaZfyF+pwq1Mq2ePM8ZRpQxFlhBIhFx438b59q04mwMKhjKSfKzVtdAYGU8b4JSj3zpVtlPNX4GaAer9eipSQw1319GVneOR79yRpUL938biXG05hP7CY0hs3LrICbQ88uDAoe/XplY2MA==; page_uid=ieavrlp0Jy0ssl5A4Jwsssssths-304147; spage_uid=ieavrlp0Jy0ssl5A4Jwsssssths-304147',
+            "dnt": "1",
+            "logic": "PART",
+            "referer": "https://search.shopping.naver.com/search/all?frm=NVSHATC&origQuery="
+            + encText
+            + "&pagingIndex="
+            + str(pageIndex)
+            + "&pagingSize="
+            + str(pageSize)
+            + "&productSet=total&query="
+            + encText
+            + "&sort=date&timestamp=&viewType=list",
+            "sbth": "6bc7553b8fa3e04779448fd212e42bb46f18162e3d9873ba0d1b41409eae93a803b742c0cb6bfdbfc7737fc581f7e8eb",
+            "sec-ch-ua": '"Whale";v="3", "Not-A.Brand";v="8", "Chromium";v="114"',
+            "sec-ch-ua-arch": '"arm"',
+            "sec-ch-ua-bitness": '"64"',
+            "sec-ch-ua-full-version-list": '"Whale";v="3.21.192.22", "Not-A.Brand";v="8.0.0.0", "Chromium";v="114.0.5735.138"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-model": '""',
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-ch-ua-platform-version": '"13.6.0"',
+            "sec-ch-ua-wow64": "?0",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Whale/3.21.192.22 Safari/537.36",
+        }
 
-    params = {
-        "eq": "",
-        "frm": "NVSHATC",
-        "iq": "",
-        "origQuery": get_store,
-        "pagingIndex": str(pageIndex),
-        "pagingSize": str(pageSize),
-        "productSet": "total",
-        "query": get_store,
-        "sort": "date",
-        "viewType": "list",
-        "xq": "",
-    }
+        params = {
+            "eq": "",
+            "frm": "NVSHATC",
+            "iq": "",
+            "origQuery": get_store,
+            "pagingIndex": str(pageIndex),
+            "pagingSize": str(pageSize),
+            "productSet": "total",
+            "query": get_store,
+            "sort": "date",
+            "viewType": "list",
+            "xq": "",
+        }
 
-    response = requests.get(
-        "https://search.shopping.naver.com/api/search/all",
-        params=params,
-        headers=headers,
-    )
-
-    items = json.loads(response.text)
-
-    msg = "storename = {}\nstopwordQuery = {}\nstrQueryType = {}".format(
-        items["shoppingResult"]["query"],
-        items["shoppingResult"]["stopwordQuery"],
-        items["shoppingResult"]["strQueryType"],
-    )
-    result += "storename = {}".format(items["shoppingResult"]["query"]) + "\n"
-    print(msg)
-
-    if len(items["shoppingResult"]["nluTerms"]) > 0:
-        print("")
-        result += "\n"
-        for i, r in enumerate(items["shoppingResult"]["nluTerms"]):
-            # print(r)
-            # msg = f'{i}={r}'
-            msg = f"{r['keyword']}'s type = {r['type']}"
-            result += msg + "\n"
-            print(msg)
-
-    if items["shoppingResult"]["total"] < 1:
-        get_mid = ""
-        print("can't searching")
-        pyautogui.alert(result + "\n\n" + "can't searhing")
-        sys.exit(0)
-    else:
-        # 1-3. MID값 찾기
-        print("")
-        msg = "store = {}'s total={}".format(
-            get_store, format(int(items["shoppingResult"]["total"]), ",")
+        response = requests.get(
+            "https://search.shopping.naver.com/api/search/all",
+            params=params,
+            headers=headers,
         )
-        result += "\n" + msg + "\n"
+
+        items = json.loads(response.text)
+
+        msg = "storename = {}\nstopwordQuery = {}\nstrQueryType = {}".format(
+            items["shoppingResult"]["query"],
+            items["shoppingResult"]["stopwordQuery"],
+            items["shoppingResult"]["strQueryType"],
+        )
+        result += "storename = {}".format(items["shoppingResult"]["query"]) + "\n"
         print(msg)
 
-        # print(len(items['shoppingResult']['products']))
-        for item in items["shoppingResult"]["products"]:
-            if get_product in item["productTitle"]:
-                get_mid = item["id"]
-                opendate = datetime.strptime(item["openDate"],'%Y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S')
-                msg = "{} product's mid = {}, opendate = {} ({}/{})".format(
-                    get_product, get_mid, opendate, item["rank"], items["shoppingResult"]["total"]
-                )
-                result += msg + "\n\n"
+        if len(items["shoppingResult"]["nluTerms"]) > 0:
+            print("")
+            result += "\n"
+            for i, r in enumerate(items["shoppingResult"]["nluTerms"]):
+                # print(r)
+                # msg = f'{i}={r}'
+                msg = f"{r['keyword']}'s type = {r['type']}"
+                result += msg + "\n"
                 print(msg)
+
+        if items["shoppingResult"]["total"] > 4000:
+            print("Too many items({}) in '{}'".format(items["shoppingResult"]["total"], get_store))
+            pyautogui.alert(result + "\n\n" + "Too many items. {} is over 4000.\nRetry other query(storename+somewords).".format(items["shoppingResult"]["total"]))
+            sys.exit(1)
+
+        if items["shoppingResult"]["total"] < 1:
+            get_mid = ""
+            print("can not searching")
+            pyautogui.alert(result + "\n\n" + "can not searching")
+            sys.exit(0)
+        else:
+            # 1-3. MID값 찾기
+            if pageIndex == 1:
+                print("")
+                msg = "store = {}'s total={}".format(
+                    get_store, format(int(items["shoppingResult"]["total"]), ",")
+                )
+                result += "\n" + msg + "\n"
+                print(msg)
+
+            # print(len(items['shoppingResult']['products']))
+            for item in items["shoppingResult"]["products"]:
+                if get_product in item["productTitle"]:
+                    got_it = True
+                    get_mid = item["id"]
+                    opendate = datetime.strptime(item["openDate"],'%Y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S')
+                    msg = "{} product's mid = {}, opendate = {} ({}/{})".format(
+                        get_product, get_mid, opendate, item["rank"], items["shoppingResult"]["total"]
+                    )
+                    result += msg + "\n\n"
+                    print(msg)
+                if got_it == True:
+                    break
+
+        pageIndex += 1
 
 if get_mid == "":
     print("no mid!")
@@ -161,7 +176,7 @@ if get_mid == "":
 
 # 1-2. MID 키워드 기록
 get_write = pyautogui.prompt(
-    "찾은 MID와 키워드를 rank list 에 기록하시겠습니까?\n(1:기록 / 0:무기록)", "상품 순위 찾기 (Step 3-1)", default=1
+    "찾은 MID와 키워드를 시트에 기록하시겠습니까?\n(1:기록 / 0:무기록)\n\n(list에 이미 기록되어 있으면 pass, rank는 현재 시간으로 기록됨)", "상품 순위 찾기 (Step 3-1)", default=1
 )
 if get_write == "1":
     msg = "시트에 기록합니다\n\n"
@@ -170,14 +185,14 @@ else:
 
 # 2-1. 입력값 받기 - (keyword)
 get_keyword = pyautogui.prompt(
-    msg+"키워드를 입력하세요", "상품 순위 찾기 (Step 3-2)", default=default_keyword
+    msg+"네이버 쇼핑에서 검색할 키워드를 입력하세요", "상품 순위 찾기 (Step 3-2)", default=default_keyword
 )
 if get_keyword == "" or get_keyword == None:
     pyautogui.alert("정상적인 입력으로 다시 시도해주세요")
     sys.exit(1)
 get_keyword = get_keyword.strip()
 
-# 2-2. 시트에 기록
+# 2-2. 시트에 기록 - rank list
 get_write_match = False
 if get_write == "1":
     try:
@@ -203,19 +218,19 @@ if get_write == "1":
 
             list_row = []
             list_row.append(1)
-            list_row.append(get_store)
+            list_row.append('')
             list_row.append(get_keyword)
             list_row.append('')
             list_row.append(get_mid)
             list_row.append('')
+            list_row.append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             list_row.append(opendate)
-            list_row.append(datetime.now().strftime("%Y. %m. %d"))
             # print(list_row)
             r = len(worksheet.col_values(2)) + 1
 
             warnings.filterwarnings(action="ignore")
             worksheet.update('A' + str(r) + ':H' + str(r), [list_row])
-            worksheet.format('E' + str(r) + ':H' + str(r), {'numberFormat':{'type':'NUMBER_FORMAT_TYPE_UNSPECIFIED'}})
+            # worksheet.format('E' + str(r) + ':H' + str(r), {'numberFormat':{'type':'NUMBER_FORMAT_TYPE_UNSPECIFIED'}})
             warnings.filterwarnings(action="default")
 
             print("{} \tfinish for google sheet writing".format(datetime.now()))
@@ -309,8 +324,8 @@ while got_it == False:
             for item in items["shoppingResult"]["products"]:
                 if get_mid == item["id"]:
                     print("")
-                    msg = "{}\t{}-mid's rank is {:3>} ({:3>}p {:>2})\n title is '{}'".format(
-                        datetime.now().strftime("%Y-%m-%d"),
+                    msg = "{}\t{} - mid's rank is {:3>} ({:3>}p {:>2})\n title is '{}'".format(
+                        datetime.now(),
                         get_mid, 
                         format(
                             int(item["rank"]),
@@ -327,8 +342,8 @@ while got_it == False:
                     got_it = True
                     product_title = item["productTitle"]
                     product_rank = int(item["rank"])
-                    if ('get_store' not in locals()) and ('get_store' not in globals()):
-                        get_store = item["mallName"]
+                    get_store = item["mallName"]
+                    opendate = datetime.strptime(item["openDate"],'%Y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S')
                     break
             pageIndex += 1
     except Exception as e:
@@ -353,7 +368,8 @@ if get_write == "1":
 
     warnings.filterwarnings(action="ignore")
     if 'r' in globals():
-        worksheet.update('I' + str(r), product_rank)
+        worksheet.update('H' + str(r) + ':I' + str(r), [[opendate, product_rank]])
+        worksheet.update('B' + str(r), get_store)
     wsheet.update('A' + str(ro) + ':H' + str(ro), [list_row])
     warnings.filterwarnings(action="default")
 
